749a727df7f25a974833957618e2a5d2464d3e79
Luciano Ramos <thalmenoch@gmail.com>
2023-08-25 01:57:24 -0300

    feito

---
+++ RNA/AV.I/1ª.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+# Função de ativação (função degrau)
+def step_function(x):
+    if x >= 0:
+        return 1
+    else:
+        return 0
+
+# Definindo os pesos e o viés para a porta and
+weights = np.array([1, 1])  # Pesos positivos
+bias = -1.5  # Viés
+
+# Função para calcular a saída do perceptron
+def perceptron(input_values, weights, bias):
+    net_input = np.dot(input_values, weights) + bias
+    output = step_function(net_input)
+    return output 
+
+# Função para testar a porta AND
+def test_and_gate():
+    input_data = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+
+    print("Input 1\tInput 2\tAND Output")
+
+    for inputs in input_data:
+        input1, input2 = inputs
+        output = perceptron(inputs, weights, bias)
+        print(f"{input1}\t{input2}\t{output}")
+
+if __name__ == "__main__":
+    test_and_gate()
---
+++ RNA/AV.I/2ª.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+# Função de ativação (função degrau)
+def step_function(x):
+    if x <= 0:
+        return 1
+    else:
+        return 0
+
+# Definindo os pesos e o viés para a porta NOR
+weights = np.array([1, 1])  # Pesos positivos para negar os valores de entrada
+bias = -0.5  # Viés
+
+# Função para calcular a saída do perceptron
+def perceptron(input_values, weights, bias):
+    net_input = np.dot(input_values, weights) + bias
+    output = step_function(net_input)
+    return output
+
+# Função para testar a porta NOR
+def test_nor_gate():
+    input_data = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+
+    print("Input 1\tInput 2\tNOR Output")
+
+    for inputs in input_data:
+        input1, input2 = inputs
+        output = perceptron(inputs, weights, bias)
+        print(f"{input1}\t{input2}\t{output}")
+
+if __name__ == "__main__":
+    test_nor_gate()
