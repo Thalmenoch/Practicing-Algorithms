6d75d69992e960b4c53f82446362a304444c6609
Luciano Ramos <thalmenoch@gmail.com>
2023-09-07 16:53:30 -0300

    1 feita

--- RNA/AV.II/1ª.py
+++ RNA/AV.II/1ª.py
@@ -1,49 +1,36 @@
 import numpy as np
 
-class Adaline:
-    def __init__(self, input_size, learning_rate=0.1, epochs=100):
-        self.learning_rate = learning_rate
-        self.epochs = epochs
-        self.weights = np.zeros(input_size + 1)  # +1 para o peso do viés (bias)
+input_size = 2
+learning_rate = 0.1
+epochs = 100
 
-    def predict(self, inputs):
-        # Função de ativação: Linear (identidade)
-        net_input = np.dot(inputs, self.weights[1:]) + self.weights[0]
-        return 1 if net_input >= 0 else 0
+weights = np.zeros(input_size + 1)
 
-    def train(self, training_data, target):
-        for _ in range(self.epochs):
-            for inputs in training_data:
-                prediction = self.predict(inputs)
-                error = target - prediction
-                self.weights[1:] += self.learning_rate * error * inputs
-                self.weights[0] += self.learning_rate * error
-
-def main():
-    # Defina os dados de treinamento para o problema "NAND"
-    training_data = np.array([
+training_data = np.array([
         [0, 0],
         [0, 1],
         [1, 0],
         [1, 1]
     ])
-    target = np.array([1, 1, 1, 0])  # Saída esperada para o NAND
 
-    # Crie e treine a Adaline
-    adaline = Adaline(input_size=2)
-    adaline.train(training_data, target)
+labels = np.array([1, 1, 1, 0])
 
-    # Teste a rede treinada
-    test_data = np.array([
+for _ in range(epochs):
+    for i in range(len(training_data)):
+        inputs = np.insert(training_data[i], 0, 1)
+        prediction = np.dot(inputs, weights)
+        error = labels[i] - (prediction >= 0)
+        weights += learning_rate * error * inputs
+
+test_inputs = np.array([
         [0, 0],
         [0, 1],
         [1, 0],
         [1, 1]
     ])
-    print("Resultado do NAND:")
-    for inputs in test_data:
-        prediction = adaline.predict(inputs)
-        print(f"Entradas: {inputs}, Saída: {prediction}")
 
-if __name__ == "__main__":
-    main()
+for inputs in test_inputs:
+    inputs_with_bias = np.insert(inputs, 0, 1)
+    summation = np.dot(inputs_with_bias, weights)
+    prediction = 1 if summation >= 0 else 0
+    print(f"Entradas: {inputs}, Saída Prevista: {prediction}")
