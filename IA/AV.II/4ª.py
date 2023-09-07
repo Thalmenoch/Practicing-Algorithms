18789b2da3abdbb2836f7c59334fcceca5c0afd2
Luciano Ramos <thalmenoch@gmail.com>
2023-09-07 14:28:46 -0300

    4 terminada

--- IA/AV.II/4ª.py
+++ IA/AV.II/4ª.py
@@ -1,16 +1,17 @@
 import numpy as np
 from sklearn.metrics import r2_score, mean_squared_error
 
-def ordenacao(x, y):
-    melhor_ordem
-    melhor_r2
-    melhor_eqm
-    for ordem in range(1, 10):  # Tente polinômios de ordem 1 a 10
-        coeficientes = np.polyfit(x, y, ordem)
-        y_previsto = np.polyval(coeficientes, x)
+def ordenacao(dados1, dados2):
+    melhor_ordem = None
+    melhor_r2 = -np.inf
+    melhor_eqm = np.inf
+
+    for ordem in range(1, 10):  
+        coeficientes = np.polyfit(dados1, dados2, ordem)
+        y_previsto = np.polyval(coeficientes, dados1)
     
-        r2 = r2_score(y, y_previsto)
-        eqm = mean_squared_error(y, y_previsto)
+        r2 = r2_score(dados2, y_previsto)
+        eqm = mean_squared_error(dados2, y_previsto)
         
         if r2 > melhor_r2:
             melhor_r2 = r2
@@ -22,3 +23,9 @@ def ordenacao(x, y):
 if __name__ == '__main__':
     x = np.array([0, 3, -1, 4, 3, 5, 2, 10, 2, 4])
     y = np.array([0.2, 0.8, 2.4, 6.5, 7.1, 7.5, 7.7, 8.1, 8.9, 10.2])
+
+    melhor_r2, melhor_ordem, melhor_eqm = ordenacao(x, y)
+
+    print(f"Melhor ordem do polinômio: {melhor_ordem}")
+    print(f"Melhor R²: {melhor_r2:.2f}")
+    print(f"Melhor EQM: {melhor_eqm:.2f}")
