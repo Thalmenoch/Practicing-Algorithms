7f842bdc81494a46b8875c48086cdd94d77fb1eb
Luciano Ramos <thalmenoch@gmail.com>
2023-09-21 19:03:31 -0300

    ajuste na q_2

--- rna/AV_III/q_2.py
+++ rna/AV_III/q_2.py
@@ -12,21 +12,22 @@ def forward_pass(X):
     
     return A2
 
-# Definição dos dados de entrada (porta NOR com 3 entradas)
-X = np.array([[0, 0, 0], [0, 0, 1], [0, 1, 0], [0, 1, 1],
-              [1, 0, 0], [1, 0, 1], [1, 1, 0], [1, 1, 1]])
+if __name__ == '__main__':
+    # Definição dos dados de entrada (porta NOR com 3 entradas)
+    X = np.array([[0, 0, 0], [0, 0, 1], [0, 1, 0], [0, 1, 1],
+                [1, 0, 0], [1, 0, 1], [1, 1, 0], [1, 1, 1]])
 
-np.random.seed(146)
-input_size = 3
-hidden_size = 2  
-output_size = 1
+    np.random.seed(146)
+    input_size = 3
+    hidden_size = 2  
+    output_size = 1
 
-W1 = np.random.normal(scale=1, size=(input_size, hidden_size))
-W2 = np.random.normal(scale=1, size=(hidden_size, output_size))
+    W1 = np.random.normal(scale=1, size=(input_size, hidden_size))
+    W2 = np.random.normal(scale=1, size=(hidden_size, output_size))
 
-output = forward_pass(X)
+    output = forward_pass(X)
 
-rounded_output = np.round(output)
+    rounded_output = np.round(output)
 
-print("Saídas arredondadas:")
-print(rounded_output)
+    print("Saídas arredondadas:")
+    print(rounded_output)
