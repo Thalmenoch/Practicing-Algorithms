4ed01bccad5dde2d8025300ad8fa47dd76898fe7
Luciano Ramos <thalmenoch@gmail.com>
2023-11-29 16:26:35 -0300

    Q_3 E Q_4
    há um problema na 4 com o n_samples e k_clusters

---
+++ RNA/AV_VI/q3.py
@@ -0,0 +1,48 @@
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.neural_network import MLPClassifier
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+# Função para gerar dados para a porta NOR
+def gerando_nor():
+    X, y = make_classification(n_samples=1000, n_features=2, n_informative=2, n_redundant=0, n_clusters_per_class=1, random_state=42)
+    y = np.logical_not(y)  # Inverte as classes para representar a porta NOR
+    return X, y
+
+# Função para treinar a RNA RBF
+def treinando_rbf(X_train, y_train, n_clusters=10):
+    # Passo 1: Clusterização usando K-Means
+    kmeans = KMeans(n_clusters=n_clusters, random_state=42)
+    kmeans.fit(X_train)
+    centers = kmeans.cluster_centers_
+
+    # Passo 2: Calcular as funções de base radial (RBF)
+    rbf_functions = np.exp(-0.1 * np.sum((X_train[:, np.newaxis] - centers) ** 2, axis=2))
+
+    # Passo 3: Treinar a camada de saída usando perceptron multicamadas (MLP)
+    rbf_mlp = MLPClassifier(hidden_layer_sizes=(n_clusters,), activation='logistic', max_iter=1000, random_state=42)
+    rbf_mlp.fit(rbf_functions, y_train)
+
+    return centers, rbf_mlp
+
+# Função para fazer previsões usando a RNA treinada
+def predicao_rbf(X, centers, rbf_mlp):
+    rbf_functions = np.exp(-0.1 * np.sum((X[:, np.newaxis] - centers) ** 2, axis=2))
+    return rbf_mlp.predict(rbf_functions)
+
+if __name__ == "__main__":
+    # Gerar dados de treinamento e teste
+    X, y = gerando_nor()
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # Treinar a RNA RBF
+    centers, rbf_mlp = treinando_rbf(X_train, y_train)
+
+    # Fazer previsões nos dados de teste
+    y_pred = predicao_rbf(X_test, centers, rbf_mlp)
+
+    # Avaliar a precisão do modelo
+    accuracy = accuracy_score(y_test, y_pred)
+    print(f'Acurácia do modelo: {accuracy:.2f}')
